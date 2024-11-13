642b90429c28186ee03c52995f8380e33f3b6136
Daniel <thecodedaniel@gmail.com>
2024-11-13 10:40:36 +0100

    feat: settings basic UI implementation

--- lib/src/core/constants/strings.dart
+++ lib/src/core/constants/strings.dart
@@ -2,6 +2,10 @@ class Strings {
   static String appName = "Flushare";
   static String flushare = "flushare";
 
+  static String flutterCredit = "Proudly Built with Flutter";
   static String sendAFile = "Send a file";
   static String receiveAFile = "Receive a file";
+  static String settings = "Settings";
+  static String storageLocation = "Storage location";
+  static String sendAFeedback = "Send a feedback";
 }
